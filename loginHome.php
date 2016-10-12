41529cc8051a71ec5dbaa7f998690096f30205c4
Marc <Marc@Marc-Laptop>
2016-10-12 12:27:34 +0200

    Begonnen aan maken komende wedstrijden e.d.

--- loginHome.php
+++ loginHome.php
@@ -4,8 +4,25 @@ if(!function_exists("isLoggedIn")){
 }
 ?>
 
-<h1>Actuele wedstrijden</h1>
+<h1>Komende wedstrijden</h1>
 <hr/>
 <table class="wedstrijdTbl">
+    <tr>
+        <th>Thuis</th>
+        <th>Uit</th>
+    </tr>
+    <?php
+    $komend = "Komend";
     
+    $pdo = new PDO("mysql:host=$DBhost;dbname=$DB", "$DBuser", "$DBpassw");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //SQL HIER AANPASSEN
+    $stmt = $pdo->prepare("SELECT * FROM dodo_wedstrijd INNER JOIN WHERE status=:st;");
+    $stmt->bindParam(":st", $komend, PDO::PARAM_STR);
+    $stmt->execute();
+    while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        
+    }
+    ?>
 </table>
