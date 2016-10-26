555f4c4e6c6808e55ce91305994ac723f8a4e446
Marc <Marc@Marc-Laptop>
2016-10-26 21:35:03 +0200

    kleine aanpassingen in css

--- loginUser.php
+++ loginUser.php
@@ -7,8 +7,10 @@ if(!function_exists("isLoggedIn")){
 <div class="innerContent">
     <img src="img/user/<?php echo $_SESSION["Afbeelding"] == NULL ? 'def.png' : $_SESSION["Afbeelding"]; ?>" id="userImg" />
     <table id="userTbl">
-        <tr><td><B>Punten:</b></td><td><?php echo $_SESSION["Punten"]; ?></td></tr>
-        <tr><td><B>Rechten:</b></td><td><?php echo $_SESSION["Admin"] == 1 ? 'Admin' : 'Normaal'; ?></td></tr>
+        <tr><td><b>Punten:</b></td></tr>
+        <tr><td><?php echo $_SESSION["Punten"]; ?></td></tr>
+        <tr><td><b>Rechten:</b></td></tr>
+        <tr><td><?php echo $_SESSION["Admin"] == 1 ? 'Admin' : 'Normaal'; ?></td></tr>
     </table>
     <button id="logoutBtn" class="redBtn" onclick="logout()">Logout</button>
 </div>
